c47cbda26fd9f9dc035364dc226b31203f9ff77f
engg-nirbhaykumar <67179429+engg-nirbhaykumar@users.noreply.github.com>
2025-11-25 09:27:09 +0530

    Time: 5 ms (52.97%), Space: 46.9 MB (7.26%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.java
+++ 0013-roman-to-integer/0013-roman-to-integer.java
@@ -1,21 +1,25 @@
 class Solution {
+
     public int romanToInt(String s) {
-        Map<Character, Integer> rmap = new HashMap<>();
-        rmap.put('I', 1);
-        rmap.put('V', 5);
-        rmap.put('X', 10);
-        rmap.put('L', 50);
-        rmap.put('C', 100);
-        rmap.put('D', 500);
-        rmap.put('M', 1000);
+        // Mapping of Roman symbols to their integer values
+        Map<Character, Integer> map = new HashMap<>();
+        map.put('I', 1);
+        map.put('V', 5);
+        map.put('X', 10);
+        map.put('L', 50);
+        map.put('C', 100);
+        map.put('D', 500);
+        map.put('M', 1000);
 
         int ans = 0;
 
+        // Traverse the string
         for (int i = 0; i < s.length(); i++) {
-            if (i + 1 < s.length() && rmap.get(s.charAt(i)) < rmap.get(s.charAt(i + 1))) {
-                ans -= rmap.get(s.charAt(i));
+            // Check if current value is less than the next one (e.g., IV = 4)
+            if (i + 1 < s.length() && map.get(s.charAt(i)) < map.get(s.charAt(i + 1))) {
+                ans -= map.get(s.charAt(i)); // Subtract in such cases
             } else {
-                ans += rmap.get(s.charAt(i));
+                ans += map.get(s.charAt(i)); // Otherwise just add normally
             }
         }
 
