3af7543bba23df4954641b14a6d26b0deea2e1a3
engg-nirbhaykumar <67179429+engg-nirbhaykumar@users.noreply.github.com>
2026-06-03 22:31:10 +0530

    Time: 0 ms (100%), Space: 46.4 MB (46.6%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
@@ -1,22 +1,14 @@
 class Solution {
     public int maxProfit(int[] prices) {
-        // This will store the total profit from multiple transactions
-        int maxProfit = 0;
+        int profit = 0;
 
-        // Start from day 1 since we compare with previous day
         for (int i = 1; i < prices.length; i++) {
-
-            // If today's price is higher than yesterday's,
-            // we can make profit by buying yesterday and selling today
+            // If current price is higher than previous, we can gain profit
             if (prices[i] > prices[i - 1]) {
-
-                // Add this profit to the total
-                // This effectively captures all upward trends
-                maxProfit += (prices[i] - prices[i - 1]);
+                profit += prices[i] - prices[i - 1];
             }
         }
 
-        // Return total accumulated profit
-        return maxProfit;
+        return profit;
     }
 }
